5d667b095ff3e54adffec080830a33c1f04c8673
matmal00 <mateusz.malec@microsoft.wsei.edu.pl>
2023-02-09 18:17:16 +0100

    feat: handle orders endpoints

---
+++ LibraryBackend/Controllers/OrdersController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryBackend.Context;
+using LibraryBackend.Models;
+using LibraryBackend.ResponseModels;
+using LibraryBackend.RequestModels;
+using LibraryBackend.Mappers;
+
+namespace LibraryBackend.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public OrdersController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderResponse>> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(order.BookId);
+            var user = await _context.Users.FindAsync(order.UserId);
+            var orderStatus = await _context.OrderStatuses.FindAsync(order.OrderStatusId);
+            var categories = await _context.Categories.ToListAsync();
+
+            return new OrderResponse()
+            {
+                Id = id,
+                Book = BookMapper.ToBookResponseModel(book, categories),
+                User = new UserSimplifiedResponse()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    Lastname = user.Lastname    
+                },
+                OrderStatus = orderStatus
+            };
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, OrderPutRequest order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
--- LibraryBackend/Models/Role.cs
+++ LibraryBackend/Models/Role.cs
@@ -7,14 +7,7 @@ namespace LibraryBackend.Models
 {
     public partial class Role
     {
-        public Role()
-        {
-            Users = new HashSet<User>();
-        }
-
         public int Id { get; set; }
         public string RoleName { get; set; }
-
-        public virtual ICollection<User> Users { get; set; }
     }
 }
