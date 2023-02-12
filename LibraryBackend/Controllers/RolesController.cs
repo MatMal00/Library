9dca31553cc96e8943cdb89e60bfc789dc9a65ba
matmal00 <mateusz.malec@microsoft.wsei.edu.pl>
2023-02-12 11:24:00 +0100

    feat: add roles

---
+++ LibraryBackend/Controllers/RolesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryBackend.Context;
+using LibraryBackend.Models;
+
+namespace LibraryBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public RolesController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Roles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Role>>> GetRoles()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        private bool RoleExists(int id)
+        {
+            return _context.Roles.Any(e => e.Id == id);
+        }
+    }
+}
