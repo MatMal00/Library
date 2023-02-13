798b0c59b9ae899d0facb0e059c7900b67cb136d
patrykluszcz <88401427+patrykluszcz@users.noreply.github.com>
2023-02-13 21:09:34 +0100

    feat: generate new rental component

--- Library/src/app/modules/library.module.ts
+++ Library/src/app/modules/library.module.ts
@@ -5,10 +5,11 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared.module';
 import { LibraryRoutingModule } from './library-routing.module';
 import { RouterModule } from '@angular/router';
+import { RentalComponent } from './rental/rental.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RentalComponent],
   imports: [CommonModule, SharedModule, LibraryRoutingModule, RouterModule],
   bootstrap: [AppComponent],
 })
