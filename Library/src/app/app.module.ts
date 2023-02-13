26410019b73d5a90834326800034a194a8295f24
patrykluszcz <88401427+patrykluszcz@users.noreply.github.com>
2023-02-13 20:56:10 +0100

    feat: create routing between modules and generate home component

--- Library/src/app/app.module.ts
+++ Library/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LibraryModule } from './modules/library.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule],
+  imports: [BrowserModule, HttpClientModule, SharedModule, AppRoutingModule, LibraryModule],
   providers: [],
   bootstrap: [AppComponent],
 })
