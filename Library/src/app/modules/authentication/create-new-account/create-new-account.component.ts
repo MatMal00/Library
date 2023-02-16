98d8ebe03a642b40bf4583ba84be9468919d0b52
patrykluszcz <88401427+patrykluszcz@users.noreply.github.com>
2023-02-16 23:27:47 +0100

    feat: add template to create user

--- Library/src/app/modules/authentication/create-new-account/create-new-account.component.ts
+++ Library/src/app/modules/authentication/create-new-account/create-new-account.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-new-account',
   templateUrl: './create-new-account.component.html',
-  styleUrls: ['./create-new-account.component.scss']
+  styleUrls: ['./create-new-account.component.scss'],
 })
 export class CreateNewAccountComponent {
+  createAccountForm: FormGroup = this._formBuilder.group(
+    {
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', Validators.required, Validators.minLength(3)],
+      email: ['', Validators.required, Validators.email],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    },
+  );
 
+  constructor(private _formBuilder: FormBuilder) {}
+
+  get form() {
+    return this.createAccountForm.controls;
+  }
 }
