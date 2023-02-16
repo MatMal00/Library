01f4f252ad09cd56294e4aecddff4612c5aca1b4
patrykluszcz <88401427+patrykluszcz@users.noreply.github.com>
2023-02-16 19:40:44 +0100

    feat: implement design for login page

--- Library/src/app/app.module.ts
+++ Library/src/app/app.module.ts
@@ -7,12 +7,30 @@ import { HomeComponent } from './modules/home/home.component';
 import { RentalComponent } from './modules/rental/rental.component';
 import { ShopComponent } from './modules/shop/shop.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RentalComponent, ShopComponent, LoginComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent, HomeComponent, RentalComponent, ShopComponent, LoginComponent],
 })
