2f5d948899c483043d0f1440ce717b20c7df6554
matmal00 <mateusz.malec@microsoft.wsei.edu.pl>
2023-02-17 22:13:51 +0100

    feat: add role for get users

--- Library/src/proxy.conf.js
+++ Library/src/proxy.conf.js
@@ -1,14 +1,6 @@
 const PROXY_CONFIG = [
   {
-    context: [
-      '/api/books',
-      '/api/books/{id}',
-      '/api/OrderStatus',
-      '/api/categories',
-      '/api/auth/register',
-      '/api/auth/login',
-      '/api/orders',
-    ],
+    context: ['/api/**'],
     target: 'https://localhost:7283',
     secure: false,
   },
